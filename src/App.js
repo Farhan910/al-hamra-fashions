8291721a067cfe20e53789aa3b81a85a9846396d
Farhan Faruk <farhanfaruk@gmail.comm>
2022-04-04 09:51:00 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Reviews from './components/Reviews/Reviews';
 import NotFound from './components/NotFound/NotFound';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [dress,setDress] = useState([]);
+
+  useEffect(() => {
+    fetch('fakeData.json')
+    .then(res => res.json())
+    .then(data => setDress(data[0]))
+  } , []);
+
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home dress={dress}/>}/>
         <Route path="/Reviews" element={<Reviews/>}/>
         <Route path="/Login" element={<Login></Login>}/>
         <Route path="*" element={<NotFound/>}/>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import Reviews from "../Reviews/Reviews";
+import "./Home.css";
 
-const Home = () => {
-    return (
-        <div>
-            <h2>Hi hlw</h2>
-            
+const Home = (props) => {
+  const {img} = props.dress;
+  return (
+    <div>
+      <div className="home">
+        <div className="home-left">
+          <h1>
+            Hi this is AL - HAMRA <br /> FASHION GALLERY
+          </h1>
         </div>
-    );
+
+        <div className="home-right">
+          <img src={img} alt="" />
+        </div>
+        
+      </div>
+      <button className="btn">hire me </button>
+
+      <Reviews></Reviews>
+    </div>
+  );
 };
 
 export default Home;
